4250ba3708815e401280d7e3feed5b71c9c0d5fc
karkkant <antti.karkkainen@live.fi>
2022-06-12 13:28:59 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# valheim-reliable_block
-Valheim mod that makes blocking more logical
+Reliable Block
+=========
+Valheim mod that makes blocking more logical.
+
+
+How does it work?
+--------
+In vanilla Valheim there are 2 situations in which your block/parry is completely ignored:
+1) When you don't have enough stamina
+2) When the damage exceeds your block armor and the exceeding damage fills your stagger treshold
+
+This mod simply removes the latter check. When you block/parry, it will always count as long as you have stamina.
+
+Why?
+--------
+Because blocking should always work if you have shield up in time. That stagger treshold check is the reason why you sometimes can parry 2-star fuling with no problem and next day they randomly one-hit you to oblivion. Now when you get hit really hard, you still get staggered and take heavy damage but you might just survive.
+
+Development
+--------
+Reference dll's are not included in the repo, you need to copy them from your valheim game library to project's /libs folder.
---
+++ ReliableBlockPlugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace ReliableBlock
+{
+    [BepInPlugin("org.bepinex.plugins.reliableblock", "ReliableBlock", "1.0.0")]
+    [BepInProcess("Valheim.exe")]
+    public class ReliableBlockPlugin : BaseUnityPlugin
+    {
+        private readonly Harmony _harmony = new Harmony("org.bepinex.plugins.reliableblock");
+
+        private void Awake()
+        {
+            _harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Humanoid))]
+        [HarmonyPatch("BlockAttack")]
+        class BlockPatch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                int removeStartIndex = 0;
+
+                try
+                {
+                    // Find variable for stagger damage- flag
+                    var flagAssignIndex = codes.FindIndex(p => p.opcode == OpCodes.Call && p.operand.ToString().Contains("AddStaggerDamage"));
+                    var staggerFlagOperand = (LocalBuilder)codes[flagAssignIndex + 1].operand;
+
+                    // Target code block that we try fix
+                    var targetBlockIndex = codes.FindIndex(p => p.opcode == OpCodes.Callvirt && p.operand.ToString().Contains("BlockDamage"));
+
+                    if (targetBlockIndex > -1)
+                    {
+                        for (var i = targetBlockIndex; i > flagAssignIndex; i--)
+                        {
+                            // Find correct variable load followed by true-check
+                            if(codes[i].IsLdloc(staggerFlagOperand))
+                            {
+                                if (codes[i + 1].opcode == OpCodes.Brtrue || codes[i + 1].opcode == OpCodes.Brtrue_S)
+                                {
+                                    removeStartIndex = i;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (removeStartIndex > 0)
+                        {
+                            codes.RemoveRange(removeStartIndex, 2);
+                            Debug.Log("Patch successful!");
+                        }
+                    }
+                } catch(Exception ex)
+                {
+                    Debug.LogError(ex.Message);
+                }
+
+                if (removeStartIndex == 0) Debug.Log("Patch failed.");
+
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
